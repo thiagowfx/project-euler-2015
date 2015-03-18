4304a42fe8f6cef24c314f8101f39ea357c26f12
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-18 18:58:41 -0300

    add 003 (cpp)

---
+++ 003/3.cpp
@@ -0,0 +1,49 @@
+/*
+ * What is the largest prime factor of the number 600851475143?
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+vector<long long int> sieve(long long int N) {
+  vector<long long int> primes;
+  vector<bool> in(N, true);
+
+  for (long long int i = 2; i <= sqrt(N); ++i) {
+    if(in[i]) {
+      for (long long int j = i * i; j <= N; j += i) {
+        in[j] = false;
+      }
+    }
+  }
+
+  for(long long int i = 2; i <= N; ++i) {
+    if(in[i]) {
+      primes.push_back(i);
+    }
+  }
+
+  return primes;
+}
+
+long long int solve(long long int N) {
+  vector<long long int> primes = sieve(sqrt(N));
+  int size = primes.size();
+
+  for (int i = size - 1; i >= 0; --i) {
+    if ( (N % primes[i]) == 0 ) {
+      return primes[i];
+    }
+  }
+  return -1;
+}
+
+int main(int argc, char *argv[]) {
+  long long int number = atoll(argv[1]);
+  printf("%lld\n", solve(number));
+
+  return 0;
+}
