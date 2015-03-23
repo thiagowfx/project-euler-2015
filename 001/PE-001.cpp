7d0d0ed06a1e45dc5aef85c04aa46d5f1c6242b1
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-23 16:59:22 -0300

    001 is optimal

--- 001/PE-001.cpp
+++ 001/PE-001.cpp
@@ -6,19 +6,38 @@
 #include <cstdlib>
 using namespace std;
 
-long long int solve(int a, int b, int limit) {
+int mdc(int a, int b) {
+  if (!b)
+    return a;
+  return mdc(b, a % b);
+}
+
+long long int solve(int a, int b, long long int limit) {
   long long int sum = 0;
-  for (int n = 0; n < limit; ++n) {
-    if (!(n % a)  || !(n % b))
-      sum += n;
-  }
+  --limit;
+
+  long long int mmc = a * b / mdc(a, b);
+
+  long long int diva = limit / a;
+  long long int divb = limit / b;
+  long long int divmmc = limit / mmc;
+
+  // a
+  sum += ((a + (a + (diva - 1) * a)) * diva) / 2;
+
+  // b
+  sum += ((b + (b + (divb - 1) * b)) * divb) / 2;
+
+  // limit
+  sum -= ((mmc + (mmc + (divmmc - 1) * mmc)) * divmmc) / 2;
+
   return sum;
 }
 
 int main(int argc, char *argv[]) {
   int a = atoi(argv[1]);
   int b = atoi(argv[2]);
-  int limit = atoi(argv[3]);
+  long long int limit = atoi(argv[3]);
 
   printf("%lld\n", solve(a, b, limit));
   return 0;
