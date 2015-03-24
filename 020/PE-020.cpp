b1f6c48bf667cf59e2bd5487c1c87422f241e47c
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-24 18:54:03 -0300

    add 020(cpp)

---
+++ 020/PE-020.cpp
@@ -0,0 +1,45 @@
+/*
+ * What is the sum of the digits of the number 21000?
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <gmpxx.h>
+#include <sstream>
+using namespace std;
+
+mpz_class factorial(mpz_class number) {
+    if (number == mpz_class("0"))
+      return mpz_class("1");
+    else
+      return number * factorial(number - 1);
+}
+
+long long int sum_digits(mpz_class number) {
+  std::stringstream ss;
+  ss << number;
+  std::string s = ss.str();
+  long long int sum = 0;
+
+  for (std::string::iterator it = s.begin(); it != s.end(); ++it) {
+    sum += (*it - '0');
+  }
+
+  return sum;
+}
+
+void solve(long long int N) {
+  std::cout << sum_digits(factorial(mpz_class(std::to_string(N)))) << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  long long int number = atoll(argv[1]);
+  solve(number);
+  return 0;
+}
