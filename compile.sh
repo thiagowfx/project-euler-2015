5c768384aaa9ca06f78045a23a1aa3dbb2c1573f
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-28 10:26:07 -0300

    improve compile.sh modularity

--- compile.sh
+++ compile.sh
@@ -5,8 +5,12 @@
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
-CC=g++
+CC="g++"
 
-CCOPTS="-std=c++11 -lgmp -lgmpxx -O2 -g -fstrict-aliasing -Wall -Wextra -Werror -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wsign-compare -Wunused -Wno-unused-parameter -Wuninitialized -Winit-self -Wpointer-arith -Wredundant-decls -Wformat-nonliteral -Wno-format-zero-length -Wno-format-y2k -Wmissing-format-attribute"
+LIBS="-lgmp -lgmpxx -lm"
 
-${CC} ${CCOPTS} "$@" -o "${@%%.*}"
+DEBUG="-g -O2"
+
+CCOPTS="-std=c++11 -fstrict-aliasing -Wall -Wextra -Werror -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wsign-compare -Wunused -Wno-unused-parameter -Wuninitialized -Winit-self -Wpointer-arith -Wredundant-decls -Wformat-nonliteral -Wno-format-zero-length -Wno-format-y2k -Wmissing-format-attribute"
+
+${CC} ${LIBS} ${DEBUG} ${CCOPTS} "$@" -o "${@%%.*}"
