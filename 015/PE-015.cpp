98f8bb9f4b0a26fd2ab9bfd7cbc86977a5b225e4
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-04-11 01:19:42 -0300

    improve 015

--- 015/PE-015.cpp
+++ 015/PE-015.cpp
@@ -2,16 +2,14 @@
  * How many such routes are there through a 20×20 grid? (Lattice paths)
  */
 
-#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <gmpxx.h>
 using namespace std;
 
-void solve(long long int N) {
+/* P^{2n}_{n,n} (permutation) */
+mpz_class solve(long long int N) {
   mpz_class answer = 1;
-
-  /* P^{2n}_{n,n} (permutation) */
 
   for (long long int i = N + 1; i <= 2 * N; ++i)
     answer *= mpz_class(std::to_string(i));
@@ -19,11 +17,11 @@ void solve(long long int N) {
   for (long long int i = 1; i <= N; ++i)
     answer /= mpz_class(std::to_string(i));
 
-  std::cout << answer << std::endl;
+  return answer;
 }
 
 int main(int argc, char *argv[]) {
   long long int number = atoll(argv[1]);
-  solve(number);
+  std::cout << solve(number) << std::endl;
   return 0;
 }
