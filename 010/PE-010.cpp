4795fc7ba849a6384352bb6023db887292940f5d
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-04-11 01:17:09 -0300

    fix 010

--- 010/PE-010.cpp
+++ 010/PE-010.cpp
@@ -2,16 +2,15 @@
  * Find the sum of all the primes below two million.
  */
 
-#include <algorithm>
 #include <cmath>
-#include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <vector>
 using namespace std;
 
-vector<long long int> sieve(long long int N) {
-  vector<long long int> primes;
-  vector<bool> in(N, true);
+std::vector<long long int> sieve(long long int N) {
+  std::vector<long long int> primes;
+  std::vector<bool> in(N + 1, true);
 
   for (long long int i = 2; i * i <= N; ++i) {
     if(in[i]) {
@@ -34,13 +33,12 @@ long long int solve(long long int N) {
   vector<long long int> primes = sieve(N);
   long long int ans = 0;
   for (vector<long long int>::iterator it = primes.begin(); it != primes.end(); ++it)
-  	ans += *it;
+    ans += *it;
   return ans;
 }
 
 int main(int argc, char *argv[]) {
   long long int number = atoll(argv[1]);
-  printf("%lld\n", solve(number));
-
+  std::cout << solve(number) << std::endl;
   return 0;
 }
